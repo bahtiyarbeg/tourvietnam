4e81375e1e2fabe917ed0a489c49728bdaeacdce
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-02-04 17:01:38 +0700

    Create tours.js

---
+++ data/tours.js
@@ -0,0 +1,98 @@
+const TOURS = [
+{
+ id:"de-luxe",
+ img:"img/de-luxe/1.jpg",
+ title:{
+  ru:"De Luxe островной тур",
+  en:"De Luxe Island Tour",
+  vi:"Tour đảo De Luxe",
+  zh:"豪华岛屿游",
+  ko:"디럭스 섬 투어",
+  fr:"Tour île De Luxe",
+  tr:"De Luxe Ada Turu"
+ },
+ time:"08:00 – 16:00",
+ take:["Купальник","Полотенце","Солнцезащитный крем"],
+ include:["Трансфер","Катер","Гид"],
+ exclude:["Личные расходы"],
+ program:[
+  "Сбор туристов",
+  "Катер",
+  "Пляж Бай Чай",
+  "Купание",
+  "Рыбацкая деревня",
+  "Возвращение"
+ ]
+},
+
+{
+ id:"vip-hon-tam-1",
+ img:"img/vip-hon-tam-1/1.jpg",
+ title:{
+  ru:"VIP тур – Хон Там 1",
+  en:"VIP Hon Tam 1",
+  vi:"VIP Hòn Tằm 1",
+  zh:"VIP 洪潭 1",
+  ko:"VIP 혼탐 1",
+  fr:"VIP Hon Tam 1",
+  tr:"VIP Hon Tam 1"
+ },
+ time:"08:00 – 16:00",
+ take:["Купальник","Полотенце"],
+ include:["Катер","Гид"],
+ exclude:["Личные расходы"],
+ program:[
+  "Пляж Бай Чай",
+  "Коралловая бухта",
+  "Рыбацкая деревня",
+  "Хон Там (грязевые ванны)"
+ ]
+},
+
+{
+ id:"robinson",
+ img:"img/robinson/1.jpg",
+ title:{
+  ru:"Робинзон тур",
+  en:"Robinson Island",
+  vi:"Đảo Robinson",
+  zh:"鲁滨逊岛",
+  ko:"로빈슨 섬",
+  fr:"Île Robinson",
+  tr:"Robinson Adası"
+ },
+ time:"08:00 – 15:30",
+ take:["Купальник"],
+ include:["Катер","Гид"],
+ exclude:["Личные расходы"],
+ program:[
+  "Остров без туристов",
+  "Снорклинг",
+  "Отдых",
+  "Купание"
+ ]
+},
+
+{
+ id:"nemo",
+ img:"img/nemo/1.jpg",
+ title:{
+  ru:"Nemo Trip",
+  en:"Nemo Trip",
+  vi:"Tour Nemo",
+  zh:"尼莫之旅",
+  ko:"니모 투어",
+  fr:"Nemo Trip",
+  tr:"Nemo Turu"
+ },
+ time:"Утро",
+ take:["Купальник"],
+ include:["Гид","Снорклинг"],
+ exclude:["Личные расходы"],
+ program:[
+  "Детский снорклинг",
+  "Рыбки",
+  "Безопасная зона"
+ ]
+}
+];
