fd9b0ca8e8236f5b29991c4d3c809161888b7038
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-02-05 13:55:34 +0700

    Update tours.js

--- data/tours.js
+++ data/tours.js
@@ -1,11 +1,19 @@
 const tours = [
   {
+    id: "de-luxe",
+    title: "De Luxe",
+    subtitle: "Премиум отдых",
+    description: "Комфортная яхта, пляжи, купание, отдых без толп.",
+    image: "img/de-luxe/1.jpg"
+  },
+  {
     id: "robinson",
     title: "Tour Robinson",
     subtitle: "Тихий остров вдали от толп",
+    description: "Уединённый остров, белый песок, релакс.",
     image: "img/robinson/1.jpg"
-  },
-  {
+  }
+];
     id: "de-luxe",
     title: "De Luxe",
     subtitle: "Премиум отдых",
