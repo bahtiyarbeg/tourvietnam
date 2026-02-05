1885ae37e4adce840e8393d9ad18c6d594810551
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-02-05 12:57:15 +0700

    Update tours.js

--- data/tours.js
+++ data/tours.js
@@ -1,92 +1,26 @@
-const tours = [
-  {
-    id: "robinson",
-    title: "Tour Robinson",
-    subtitle: "Тихий остров вдали от толп",
-    image: "img/robinson/1.jpg"
-  },
-  {
-    id: "de-luxe",
-    title: "De Luxe",
-    subtitle: "Премиум отдых",
-    image: "img/de-luxe/1.jpg"
-  },
-  {
-    id: "diving",
-    title: "Diving",
-    subtitle: "Дайвинг",
-    image: "img/diving/1.jpg"
-  },
-  {
-    id: "four-islands-ninh-thuan",
-    title: "4 острова",
-    subtitle: "Нинь Туан",
-    image: "img/four-islands-ninh-thuan/1.jpg"
-  },
-  {
-    id: "hon-mun-hon-tam",
-    title: "Hon Mun + Hon Tam",
-    subtitle: "Снорклинг",
-    image: "img/hon-mun-hon-tam/1.jpg"
-  },
-  {
-    id: "hon-mun-mini-beach",
-    title: "Hon Mun Mini Beach",
-    subtitle: "Пляж",
-    image: "img/hon-mun-mini-beach/1.jpg"
-  },
-  {
-    id: "hon-mun-vai-chai",
-    title: "Vai Chai",
-    subtitle: "Остров",
-    image: "img/hon-mun-vai-chai/1.jpg"
-  },
-  {
-    id: "mini-beach-hon-tam",
-    title: "Mini Beach",
-    subtitle: "Отдых",
-    image: "img/mini-beach-hon-tam/1.jpg"
-  },
-  {
-    id: "nemo",
-    title: "Nemo",
-    subtitle: "Рыбки",
-    image: "img/nemo/1.jpg"
-  },
-  {
-    id: "one-island-hon-tam",
-    title: "1 остров Hon Tam",
-    subtitle: "Классика",
-    image: "img/one-island-hon-tam/1.jpg"
-  },
-  {
-    id: "one-island-mini-beach",
-    title: "1 остров Mini",
-    subtitle: "Спокойно",
-    image: "img/one-island-mini-beach/1.jpg"
-  },
-  {
-    id: "snorkeling-boat",
-    title: "Snorkeling Boat",
-    subtitle: "Лодка",
-    image: "img/snorkeling-boat/1.jpg"
-  },
-  {
-    id: "vineyard-ninh-thuan",
-    title: "Vineyard",
-    subtitle: "Виноградники",
-    image: "img/vineyard-ninh-thuan/1.jpg"
-  },
-  {
-    id: "vip-hon-tam-1",
-    title: "VIP Hon Tam 1",
-    subtitle: "VIP",
-    image: "img/vip-hon-tam-1/1.jpg"
-  },
-  {
-    id: "vip-hon-tam-2",
-    title: "VIP Hon Tam 2",
-    subtitle: "VIP",
-    image: "img/vip-hon-tam-2/1.jpg"
-  }
-];
+{
+  id: "de-luxe",
+  title: "De Luxe",
+  subtitle: "Премиум отдых",
+  image: "img/de-luxe/1.jpg",
+
+  time: "08:00 – 16:00",
+  guide: "English-speaking guide",
+  bring: [
+    "Купальник",
+    "Полотенце",
+    "Солнцезащитный крем",
+    "Головной убор"
+  ],
+  included: [
+    "Трансфер",
+    "Катер",
+    "Обед",
+    "Снорклинг",
+    "Гид"
+  ],
+  notIncluded: [
+    "Личные расходы",
+    "Алкоголь"
+  ]
+}
