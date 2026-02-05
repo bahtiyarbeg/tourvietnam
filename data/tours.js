10be406e0f5d413b1d9a03b5718408fe340782e3
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-02-05 12:48:13 +0700

    Update tours.js

--- data/tours.js
+++ data/tours.js
@@ -1,4 +1,4 @@
-window.tours = [
+const tours = [
   {
     id: "robinson",
     title: "Tour Robinson",
@@ -22,5 +22,71 @@ window.tours = [
     title: "4 острова",
     subtitle: "Нинь Туан",
     image: "img/four-islands-ninh-thuan/1.jpg"
+  },
+  {
+    id: "hon-mun-hon-tam",
+    title: "Hon Mun + Hon Tam",
+    subtitle: "Снорклинг",
+    image: "img/hon-mun-hon-tam/1.jpg"
+  },
+  {
+    id: "hon-mun-mini-beach",
+    title: "Hon Mun Mini Beach",
+    subtitle: "Пляж",
+    image: "img/hon-mun-mini-beach/1.jpg"
+  },
+  {
+    id: "hon-mun-vai-chai",
+    title: "Vai Chai",
+    subtitle: "Остров",
+    image: "img/hon-mun-vai-chai/1.jpg"
+  },
+  {
+    id: "mini-beach-hon-tam",
+    title: "Mini Beach",
+    subtitle: "Отдых",
+    image: "img/mini-beach-hon-tam/1.jpg"
+  },
+  {
+    id: "nemo",
+    title: "Nemo",
+    subtitle: "Рыбки",
+    image: "img/nemo/1.jpg"
+  },
+  {
+    id: "one-island-hon-tam",
+    title: "1 остров Hon Tam",
+    subtitle: "Классика",
+    image: "img/one-island-hon-tam/1.jpg"
+  },
+  {
+    id: "one-island-mini-beach",
+    title: "1 остров Mini",
+    subtitle: "Спокойно",
+    image: "img/one-island-mini-beach/1.jpg"
+  },
+  {
+    id: "snorkeling-boat",
+    title: "Snorkeling Boat",
+    subtitle: "Лодка",
+    image: "img/snorkeling-boat/1.jpg"
+  },
+  {
+    id: "vineyard-ninh-thuan",
+    title: "Vineyard",
+    subtitle: "Виноградники",
+    image: "img/vineyard-ninh-thuan/1.jpg"
+  },
+  {
+    id: "vip-hon-tam-1",
+    title: "VIP Hon Tam 1",
+    subtitle: "VIP",
+    image: "img/vip-hon-tam-1/1.jpg"
+  },
+  {
+    id: "vip-hon-tam-2",
+    title: "VIP Hon Tam 2",
+    subtitle: "VIP",
+    image: "img/vip-hon-tam-2/1.jpg"
   }
 ];
